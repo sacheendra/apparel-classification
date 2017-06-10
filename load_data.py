3d9afeb6108f864892d48c2b6b95237ed2501c2a
Sacheendra Talluri <sacheendra.t@gmail.com>
2017-06-10 14:40:39 +0200

    add loading partial data

--- load_data.py
+++ load_data.py
@@ -8,28 +8,44 @@ import h5py
 
 class ImageLoader(object):
 	"""load train and test data"""
-	def __init__(self, folderpath):
+	def __init__(self, folderpath, loadall=False):
 		super(ImageLoader, self).__init__()
 		testbinpath = 'test.npy'
 		trainbinpath = 'train.npy'
+		partialtestbinpath = 'partialtest.npy'
+		partialtrainbinpath = 'partialtrain.npy'
 
 		self.folderpath = folderpath
 
-		if os.path.isfile(os.path.join(folderpath, testbinpath)):
+		if ~loadall and os.path.isfile(os.path.join(folderpath, partialtestbinpath)):
+			self.test_images = self.load_data(partialtestbinpath)
+			print "partial test data loaded from numpy file"
+		elif loadall and os.path.isfile(os.path.join(folderpath, testbinpath)):
 			self.test_images = self.load_data(testbinpath)
 			print "test data loaded from numpy file"
+		elif ~loadall and os.path.isfile(os.path.join(folderpath, testbinpath)):
+			self.test_images = self.load_data(testbinpath)[:100]
+			self.store_data(self.test_images, partialtestbinpath)
 		else:
 			self.test_filenames = pd.read_csv(os.path.join(folderpath, 'test.txt'))
 			self.test_images = self.load_images(self.test_filenames)
 			self.store_data(self.test_images, testbinpath)
+			self.store_data(self.test_images[:100], partialtestbinpath)
 
-		if os.path.isfile(os.path.join(folderpath, trainbinpath)):
+		if ~loadall and os.path.isfile(os.path.join(folderpath, partialtrainbinpath)):
+			self.train_images = self.load_data(partialtrainbinpath)
+			print "partial train data loaded from numpy file"
+		elif loadall and os.path.isfile(os.path.join(folderpath, trainbinpath)):
 			self.train_images = self.load_data(trainbinpath)
 			print "train data loaded from numpy file"
+		elif ~loadall and os.path.isfile(os.path.join(folderpath, trainbinpath)):
+			self.train_images = self.load_data(trainbinpath)[:500]
+			self.store_data(self.train_images, partialtrainbinpath)
 		else:
 			self.train_filenames = pd.read_csv(os.path.join(folderpath, 'train.txt'))
 			self.train_images = self.load_images(self.train_filenames)
 			self.store_data(self.train_images, trainbinpath)
+			self.store_data(self.train_images[:500], partialtrainbinpath)
 
 	def load_images(self, filenames):
 		images = [np.ndarray(np.ndarray(0))]*filenames.shape[0]
@@ -44,6 +60,12 @@ class ImageLoader(object):
 
 	def load_data(self, filename):
 		return np.load(os.path.join(self.folderpath, filename))
+
+	def get_test_data(self):
+		return self.test_data
+
+	def get_train_data(self):
+		return self.train_data
 
 
 def main():
